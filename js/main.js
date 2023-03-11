726f94e602b59c76dcd83bd3f8848042d1812bb7
김가든 <oyasumiee2@gmail.com>
2023-03-11 23:23:37 +0900

    23-03-11 commit

--- js/main.js
+++ js/main.js
@@ -18,17 +18,30 @@ searchInputEl.addEventListener("blur", () => {
 const badgeEl = document.querySelector("header .badges");
 
 // _.throttle(콜백함수, 시간)
-window.addEventListener("scroll", _.throttle( () => {
-  console.log(window.scrollY)
-  if (window.scrollY > 500) {
-    gsap.to(badgeEl, .6, {
-      opacity: 0,
-      display: 'none'
-    });
-  } else {
-    gsap.to(badgeEl, .6, {
-      opacity: 1,
-      display: 'block'
+window.addEventListener(
+  "scroll",
+  _.throttle(() => {
+    console.log(window.scrollY);
+    if (window.scrollY > 500) {
+      gsap.to(badgeEl, 0.6, {
+        opacity: 0,
+        display: "none",
+      });
+    } else {
+      gsap.to(badgeEl, 0.6, {
+        opacity: 1,
+        display: "block",
+      });
+    }
+  }, 300)
+);
+
+// FADE - IN
+// gsap.to(요소, 지속시간(초단위), 옵션)
+const fadeEls = document.querySelectorAll(".visual .fade-in");
+fadeEls.forEach((fadeEl, index) => {
+  gsap.to(fadeEl, 1, {
+    delay: (index + 1) * 0.7,
+    opacity: 1,
   });
-}
-}, 300));
+});
