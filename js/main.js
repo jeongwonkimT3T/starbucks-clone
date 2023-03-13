e494272c9c3a46337b23b69cc12e291ec00d8832
김가든 <oyasumiee2@gmail.com>
2023-03-14 01:22:37 +0900

    23-03-14 commit

--- js/main.js
+++ js/main.js
@@ -47,40 +47,65 @@ fadeEls.forEach((fadeEl, index) => {
 });
 
 //new Swiper(선택자, 옵션)
-new Swiper('.notice-line .swiper-container', {
-  direction: 'vertical',
+new Swiper(".notice-line .swiper-container", {
+  direction: "vertical",
   autoplay: true,
-  loop: true
+  loop: true,
 });
 
-new Swiper('.promotion .swiper-container', {
+new Swiper(".promotion .swiper-container", {
   slidesPerView: 3,
   spaceBetween: 10,
   centeredSlides: true,
   loop: true,
   autoplay: {
-    delay: 5000
+    delay: 5000,
   },
   pagination: {
-    el: '.promotion .swiper-pagination', //페이지 번호 요소 선택자
-    clickable: true
+    el: ".promotion .swiper-pagination", //페이지 번호 요소 선택자
+    clickable: true,
   },
   navigation: {
-    prevEl: '.promotion .swiper-prev',
-    nextEl: '.promotion .swiper-next'
-  }
+    prevEl: ".promotion .swiper-prev",
+    nextEl: ".promotion .swiper-next",
+  },
 });
 
-
-const promotionEl = document.querySelector('.promotion');
-const promotionToggleBtn = document.querySelector('.toggle-promotion');
+const promotionEl = document.querySelector(".promotion");
+const promotionToggleBtn = document.querySelector(".toggle-promotion");
 let isHidePromotion = false;
-promotionToggleBtn.addEventListener('click', () => {
-  isHidePromotion = !isHidePromotion
+promotionToggleBtn.addEventListener("click", () => {
+  isHidePromotion = !isHidePromotion;
 
   if (isHidePromotion) {
-    promotionEl.classList.add('hide');
+    promotionEl.classList.add("hide");
   } else {
-    promotionEl.classList.remove('hide');
+    promotionEl.classList.remove("hide");
   }
 });
+
+// 범위 랜덤 함수(소수점 2자리까지)
+function random(min, max) {
+  // `.toFixed()`를 통해 반환된 문자 데이터를,
+  // `parseFloat()`을 통해 소수점을 가지는 숫자 데이터로 변환
+  return parseFloat((Math.random() * (max - min) + min).toFixed(2));
+}
+
+function floatingObject(selector, delay, size) {
+  gsap.to(
+    selector, // 선택자
+    random(1, 5, 2.5), // 애니메이션 동작 시간
+    {
+      // 옵션
+      y: size,
+      repeat: -1,
+      yoyo: true,
+      ease: Power1.easeInOut,
+      delay: random(0, delay),
+    }
+  );
+}
+
+floatingObject(".floating1", 1, 15);
+floatingObject(".floating2", 0.5, 15);
+floatingObject(".floating3", 1.5, 20);
